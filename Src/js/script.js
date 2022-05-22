ac87bca373e59d8d292884b607b124f94424b2c0
Anton <finlazeeshund@gmail.com>
2022-05-22 14:15:46 +0300

    first commit

---
+++ Src/js/script.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+  $('.carousel__ineer').slick({
+    speed: 300,
+    prevArrow: '<button type="button" class="slick-prev"><img src="/img/chevronleft.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="/img/chevronright.png"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      },
+    ]
+
+  }
+  );
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
+    })
+
+  }
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+
+
+
+  //modal okna
+
+  $('[data-modal=consultation]').on('click', function () {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+
+  $('.modal__close').on('click', function () {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+  });
+
+
+
+  $('.button_mini').each(function (i) {    //Часть кода отвечающая за смену названия пульсометра в модальном окне при нажатие клавиши купить
+    $(this).on('click', function () {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay,  #order').fadeIn('slow');
+    })
+  });
+
+
+
+  function validateForms(form){
+    $(form).validate({
+    rules: {
+      name: "required",
+      phone: "required",
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      name: "Пожалуйста введите свое имя",
+      phone: "Пожалуйста введите свой номер телефона",
+      email: {
+        required: "Пожалуйста введите свой емейл",
+        email: "Неправильный формат"
+      }
+    }
+  });
+};
+
+validateForms('#consultation-form');
+validateForms('#consultation form');
+validateForms('#order form');
+
+
+$('input[name=phone]').mask("+380 (99) 999-99-99");
+
+
+
+
+});
